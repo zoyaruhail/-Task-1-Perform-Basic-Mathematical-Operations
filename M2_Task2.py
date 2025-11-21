e61c781885ed1617204b6d41ef158806a5814e2e
Zoya Ruhail <109350804+zoyaruhail@users.noreply.github.com>
2025-11-21 22:40:34 +0530

    Add files via upload

---
+++ M2_Task2.py
@@ -0,0 +1,8 @@
+"User Inputs"
+
+first_name = input("Enter your First name: ")
+last_name = input("Enter your Last name: ")
+
+"Print Greetings"
+
+print("Hello," , first_name + " " + last_name + "!" , "Welcome to the Python Program.")
