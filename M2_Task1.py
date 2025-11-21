065b53a9f37d1bdc51584e404fb15ba966f7bc03
Zoya Ruhail <109350804+zoyaruhail@users.noreply.github.com>
2025-11-21 22:31:23 +0530

    Add files via upload

---
+++ M2_Task1.py
@@ -0,0 +1,24 @@
+""" User Inputs"""
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter your second number: "))
+
+#Addition
+
+add = num1 + num2
+print("Addition: ",add)
+
+#subtraction
+
+sub = num1 - num2
+print("Subtraction: ",sub)
+
+#Multiplication
+
+mul = num1 * num2
+print("Multiplication: ",mul)
+
+#division
+
+div = num1 / num2
+print("Division: ",div)
